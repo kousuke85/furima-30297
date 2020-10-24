377cdf70ba0f462ded6e4ef43b2e607437b26cdc
kousuke85 <meronmilk0829@gmail.com>
2020-10-24 13:39:58 +0900

    ユーザー管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :purchase_records
+
+  validates :encrypted_password, presence: true, length: { minimum: 6 }
+
 end
